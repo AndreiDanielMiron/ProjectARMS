9d9109ec37781ee94c0d6576ca9e774158cbfe68
Andrei2498 <48515205+Andrei2498@users.noreply.github.com>
2020-04-25 20:21:17 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -37,24 +37,27 @@ def split_list(lista, size):
 
 
 def get_top_posts(subreddit, time_period='day'):
-    lastday_subreddit = list(reddit.subreddit(subreddit).top(time_period))
-    print(lastday_subreddit[-1].id)
-    with open(generate_file_name(subreddit, "Top"), "w", encoding="utf-8") as csv_file:
-        writer = csv.writer(csv_file, delimiter='/')
-        writer.writerow(["Subreddit", "Title", "ID", "Score", "Upvote Ratio"])
-        for lista in split_list(lastday_subreddit, 10):
-            start_time = time.time()
-            for submission in lista:
-                line = list()
-                line.append(submission.subreddit.display_name)
-                line.append(submission.title)
-                line.append(submission.id)
-                line.append(submission.score)
-                line.append(submission.upvote_ratio)
-                writer.writerow(line)
-            end_time = time.time()
-            print(end_time - start_time)
-            time.sleep(60 - (end_time - start_time))
+    try:
+        lastday_subreddit = list(reddit.subreddit(subreddit).top(time_period))
+        print(lastday_subreddit[-1].id)
+        with open(generate_file_name(subreddit, "Top"), "w", encoding="utf-8") as csv_file:
+            writer = csv.writer(csv_file, delimiter='/')
+            writer.writerow(["Subreddit", "Title", "ID", "Score", "Upvote Ratio"])
+            for lista in split_list(lastday_subreddit, 10):
+                start_time = time.time()
+                for submission in lista:
+                    line = list()
+                    line.append(submission.subreddit.display_name)
+                    line.append(submission.title)
+                    line.append(submission.id)
+                    line.append(submission.score)
+                    line.append(submission.upvote_ratio)
+                    writer.writerow(line)
+                end_time = time.time()
+                print(end_time - start_time)
+                time.sleep(60 - (end_time - start_time))
+    except Exception as e:
+        print(e)
 
 
 def populate_today_top(list_of_subreddits):
@@ -62,7 +65,7 @@ def populate_today_top(list_of_subreddits):
         try:
             print(50 * "*")
             print(subreddit.title)
-            get_top_posts(subreddit.title)
+            get_top_posts(subreddit.display_name)
         except Exception as e:
             print(e)
 
@@ -72,6 +75,12 @@ def start_data_extraction():
 
 
 start_data_extraction()
+# get_top_posts("The Witcher")
+# print(reddit.subreddit("The Witcher").display_name)
+# print(list(reddit.subreddit("witcher").top(limit = 10)))
+# list(reddit.subreddit(subreddit).top(time_period))
+# popular_subreddits = list(reddit.subreddits.popular())
+# print(popular_subreddits)
 
 # popular_subreddits = list(reddit.subreddits.popular())
 # for i in popular_subreddits:
